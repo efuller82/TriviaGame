8a012eab594613c369a106f81574b86a6de30b6f
Eric Fuller <efuller82@gmail.com>
2019-06-21 10:58:10 -0500

    trivia questions created

---
+++ assets/javascript/app.js
@@ -0,0 +1,110 @@
+var myQuotes = [
+    {
+        quote: "I wondered why the frisbee was getting bigger, and then it hit me.",
+        answers: {
+            a: "Paul Reiser",
+            b: "Demetri Martin",
+            c: "Mitch Hedberg",
+            d: "Daniel Tosh"
+        },
+        correctAnswer: "c" 
+    },
+    
+    {
+        quote: "...when we came to America, it was so nice. It was just Indians. And they weren’t even Indians. We called them that by accident. And we still call them that!",
+        answers: {
+            a: "Louis C.K.",
+            b: "Mike Birbiglia",
+            c: "Aziz Ansari",
+            d: "John Mulaney"
+        },
+        correctAnswer: "a"
+    },
+
+    {
+        quote: "I think the most horrible name for a crime has to be manslaughter...'I slaughtered a man! Just like a pig! Put him on a spit and put an apple in his mouth!'",
+        answers: {
+            a: "Lewis Black",
+            b: "Patton Oswalt",
+            c: "Steven Wright",
+            d: "Brian Regan"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        quote: "I busted a mirror and got seven years of bad luck, but my lawyer thinks he can get me five.",
+        answers: {
+            a: "Bill Burr",
+            b: "Robin Williams",
+            c: "Steven Wrigth",
+            d: "Doug Stanhope"
+        },
+        correctAnswer: "d"
+    },
+
+    {
+        quote: "...if a bullet cost five thousand dollars, there would be no more innocent bystanders",
+        answers: {
+            a: "Richard Pryor",
+            b: "Mark Maron",
+            c: "Crhis Rock",
+            d: "Ralphy May"
+        },
+        correctAnswer: "c"
+    },
+
+    {
+        quote: "I'd rather send out a mass email than hang posters all over the place.",
+        answers: {
+            a: "Norm MacDonald",
+            b: "Todd Barry",
+            c: "Pete Holmes",
+            d: "Adam Sandler"
+        },
+        correctAnswer: "b"
+    },
+
+    {
+        quote: "My ex-girlfriend had a really weird fetish. She used to like dress up as herself and then act like a fuckin bitch all the time.",
+        answers: {
+            a: "Brian Posehn",
+            b: "Nick Swardson",
+            c: "Bo Burnham",
+            d: "Christopher Titus"
+        },
+        correctAnswer: "c"
+    },
+    {
+       quote: "I donated blood today. That's what I call getting an AIDS test.",
+       answers: {
+           a: "Sarah Silverman",
+           b: "Tig Notaro",
+           c: "Chelsea Handler",
+           d: "Amy Schumer"
+       },
+       correctAnswer: "d"
+    },
+
+    {
+        quote: "I like burritos more than Jesus because steak burritos are delicious and they're real.",
+        answers: {
+            a: "Hannibal Buress",
+            b: "Jerry Seinfeld",
+            c: "Dave Chappelle",
+            d: "Jim Carrey"
+        },
+        correctAnswer: "a",
+    },
+
+    {
+        quote: "The robe is a lazy man's tuxedo.",
+        answers: {
+            a: "David Letterman",
+            b: "Dane Cook",
+            c: "Jim Gaffigan",
+            d: "Bill Cosby"
+        },
+        correctAnswer: "c",
+    }
+];
